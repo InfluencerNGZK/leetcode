d8d7e3f52a2f85e98f17ef0e7208ebbb006aceb4
InfluencerNGZK <jasonxing_380a@foxmail.com>
2021-03-26 10:44:59 +0800

    Add solution for binary tree traversal

---
+++ Binary Tree Preorder Traversal/solution.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+if root == nil {
+return nil
+}
+nums := []int{}
+nums = append(nums, root.Val)
+nums = append(nums, preorderTraversal(root.Left)...)
+nums = append(nums, preorderTraversal(root.Right)...)
+return nums
+}
